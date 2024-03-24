10c149fd1836cb027752028fbbb60158622c4433
Dan <mpekoo1@gmail.com>
2024-03-24 20:09:47 +0300

    Add rakefile

--- Gemfile
+++ Gemfile
@@ -1,3 +1,12 @@
 source "https://rubygems.org"
 
 gem "sinatra"
+gem "pg"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "sinatra-contrib"
+
+group :development do
+	gem "tux"
+end
+
--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+require 'pg'
+
+set :database, {adapter: 'postgresql', host: 'localhost', database: 'PizzaShop', username: 'postgres', password: 'postgres', port: '5432'}
+
+class Product < ActiveRecord::Base  
+end
 
 get '/' do
 	erb :index			
